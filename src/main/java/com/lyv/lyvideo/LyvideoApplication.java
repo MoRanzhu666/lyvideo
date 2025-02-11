4967e27ce10acd62e52a8914cba6eedca9eb5048
luoying <3358778839@qq.com>
2025-02-11 11:33:34 +0800

    项目初始化

---
+++ src/main/java/com/lyv/lyvideo/LyvideoApplication.java
@@ -0,0 +1,13 @@
+package com.lyv.lyvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyvideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyv/lyvideo/LyvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyv.lyvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyvideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
